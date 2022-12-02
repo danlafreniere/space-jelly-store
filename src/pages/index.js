80751e694827cc4237e9f995a0787da9ef192ae3
Dan <lafreniere.dj@gmail.com>
2022-12-01 22:29:00 -0800

    Dynamic data fetching

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,31 @@
-import Head from 'next/head'
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
 
-import Layout from '@components/Layout';
-import Container from '@components/Container';
-import Button from '@components/Button';
+import Layout from "@components/Layout";
+import Container from "@components/Container";
+import Button from "@components/Button";
 
-import products from '@data/products';
+import products from "@data/products";
 
-import styles from '@styles/Page.module.scss'
+import styles from "@styles/Page.module.scss";
 
-export default function Home() {
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+export default function Home({ homePage }) {
+  console.log(homePage);
+  const { heroLink, heroText, heroTitle, heroBackground } = homePage;
   return (
     <Layout>
+      }
       <Head>
         <title>Space Jelly Gear</title>
         <meta name="description" content="Get your Space Jelly gear!" />
       </Head>
-
       <Container>
         <h1 className="sr-only">Space Jelly Gear</h1>
 
@@ -24,10 +33,14 @@ export default function Home() {
           <Link href="#">
             <a>
               <div className={styles.heroContent}>
-                <h2>Prepare for liftoff.</h2>
-                <p>Apparel that&apos;s out of this world!</p>
+                <h2>{heroTitle}</h2>
+                <p>{heroText}</p>
               </div>
-              <img className={styles.heroImage} src="/images/space-jelly-gear-banner.jpg" alt="" />
+              <img
+                className={styles.heroImage}
+                src={heroBackground.url}
+                alt=""
+              />
             </a>
           </Link>
         </div>
@@ -35,32 +48,64 @@ export default function Home() {
         <h2 className={styles.heading}>Featured Gear</h2>
 
         <ul className={styles.products}>
-          {products.slice(0, 4).map(product => {
+          {products.slice(0, 4).map((product) => {
             return (
               <li key={product.id}>
                 <Link href="#">
                   <a>
                     <div className={styles.productImage}>
-                      <img width="500" height="500" src={product.image} alt="" />
+                      <img
+                        width="500"
+                        height="500"
+                        src={product.image}
+                        alt=""
+                      />
                     </div>
-                    <h3 className={styles.productTitle}>
-                      { product.name }
-                    </h3>
-                    <p className={styles.productPrice}>
-                      ${ product.price }
-                    </p>
+                    <h3 className={styles.productTitle}>{product.name}</h3>
+                    <p className={styles.productPrice}>${product.price}</p>
                   </a>
                 </Link>
                 <p>
-                  <Button>
-                    Add to Cart
-                  </Button>
+                  <Button>Add to Cart</Button>
                 </p>
               </li>
-            )
+            );
           })}
         </ul>
       </Container>
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://api-us-west-2.hygraph.com/v2/clahfh7mf0p0e01t29ja4d9ep/master",
+    cache: new InMemoryCache(),
+  });
+
+  const data = await client.query({
+    query: gql`
+      query PageHome {
+        page(where: { slug: "home" }) {
+          id
+          heroLink
+          heroText
+          heroTitle
+          name
+          heroBackground {
+            height
+            url
+            width
+          }
+        }
+      }
+    `,
+  });
+
+  const homePage = data.data.page;
+  return {
+    props: {
+      homePage,
+    },
+  };
 }
